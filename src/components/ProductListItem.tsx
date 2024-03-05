6b2e155acb24b63b0930bb488a7f6d24335292c8
julianojosoa13 <josoa.juliano.rahajaharimanga@esti.mg>
2024-03-05 15:52:09 +0300

    Default image if there isn't any

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,19 +1,18 @@
 import Colors from '@/src/constants/Colors';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+import { Product } from '@/types';
+
+export const defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png"
+
 interface ProductListItemProps {
-    product : {
-        id: number,
-        image: string,
-        name: string,
-        price: number,
-    }
+    product : Product
 }
 
 const  ProductListItem = ({product}: ProductListItemProps) => {
     return (
         <View style={styles.container}>
-        <Image source={{uri: product.image}} style={styles.image}/>
+        <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image}/>
         <Text style={styles.title}>{product.name}</Text>
         <Text style={styles.price}>${product.price}</Text>
     </View>
